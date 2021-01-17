9b41cc65119aa424f6aa4b11e19cd17e11c2402d
Sambhav Kumar Thakur <sambhavkumar228@gmail.com>
2021-01-17 19:01:28 +0530

    code

--- first-word.py
+++ first-word.py
@@ -1,25 +1,14 @@
 def first_word(text: str) -> str:
-    """
-        returns the first word in a given text.
-    """
-    # your code here
-    if text == 'hi':
-        return 'hi'
-    
-    import re
-    x = re.split('\s|(?<!\d)[,.](?!\d)', text)
-
-
-    for index in range(len(x)-1):
-        if not x[index] == '':
-            return x[index]
+    w = text.split(' ')
+    return w[0]
 
 
 if __name__ == '__main__':
     print("Example:")
     print(first_word("Hello world"))
-    print(first_word('... itd...'))
-    print(first_word("don't touch"))
-    print('hoi.bio')
-
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word("a word") == "a"
+    assert first_word("hi") == "hi"
     print("Coding complete? Click 'Check' to earn cool rewards!")
